9dfca17f119d740245f0f7c0a51273250b570bf4
arulkumaran456 <65466033+arulkumaran456@users.noreply.github.com>
2023-04-24 15:50:06 +0530

    0.1.0 (#6)
    * Update handler.lua
    
    * Update schema.lua
    
    * Update handler.lua
    
    * Update handler.lua
    
    * Update handler.lua
    
    * Update handler.lua
    
    * Update handler.lua
    
    * Update handler.lua
    
    * schema changes
    
    * Update schema.lua
    
    * Update schema.lua

--- kong/plugins/soap-transform/schema.lua
+++ kong/plugins/soap-transform/schema.lua
@@ -1,10 +1,34 @@
-return {
-  no_consumer = false, -- this plugin is available on APIs as well as on Consumers,
+local typedefs = require "kong.db.schema.typedefs"
+
+
+local PLUGIN_NAME = "soap-transform"
+
+
+local schema = {
+  name = PLUGIN_NAME,
   fields = {
-    ignore_content_type = {type = "boolean", default = false},
-    
+    -- the 'fields' array is the top-level entry with fields defined by Kong
+    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
+    { protocols = typedefs.protocols_http },
+    { config = {
+        -- The 'config' record is the custom part of the plugin schema
+        type = "record",
+        fields = {
+          -- a standard defined field (typedef), with some customizations
+          { request_header = typedefs.header_name {
+              required = false,
+              default = "Arulkumar" } },  
+           { ignore_content_type = typedefs.header_name {
+              required = false,
+              default = "json" } },
+        },
+        entity_checks = {
+          -- We specify that both header-names cannot be the same
+          { distinct = { "request_header", "ignore_content_type"} },
+        },
+      },
+    },
   },
-  self_check = function(schema, plugin_t, dao, is_updating)
-    return true
-  end
 }
+
+return schema
