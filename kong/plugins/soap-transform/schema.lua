02c097a4a00e20287e78602ebb088a37fc9ac5b1
arulkumaran456 <65466033+arulkumaran456@users.noreply.github.com>
2023-04-24 15:39:58 +0530

    Update schema.lua

--- kong/plugins/soap-transform/schema.lua
+++ kong/plugins/soap-transform/schema.lua
@@ -1,22 +1,10 @@
-local typedefs = require "kong.db.schema.typedefs"
-local PLUGIN_NAME = "soap-transform"
-local schema = {
-  name = PLUGIN_NAME,
+return {
+  no_consumer = false, -- this plugin is available on APIs as well as on Consumers,
   fields = {
-    { consumer = typedefs.no_consumer },
-    { protocols = typedefs.protocols_http },
-    { config = {
-        type = "record",
-        fields = {
-          { ignore_content_type = typedefs.header_name {
-              required = false,
-              type = "boolean",
-              default = false } },
-        },
-        entity_checks = { 
-        },
-      },
-    },
+    ignore_content_type = {type = "boolean", default = false},
+    
   },
+  self_check = function(schema, plugin_t, dao, is_updating)
+    return true
+  end
 }
-return schema
