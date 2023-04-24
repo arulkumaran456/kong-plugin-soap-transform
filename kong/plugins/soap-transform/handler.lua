9ba0fa678cf5f68665d916fd8d66c7db7ff4f8f6
arulkumaran456 <65466033+arulkumaran456@users.noreply.github.com>
2023-04-24 14:37:44 +0530

    Update handler.lua

--- kong/plugins/soap-transform/handler.lua
+++ kong/plugins/soap-transform/handler.lua
@@ -1,23 +1,15 @@
+local kong = kong
 local plugin_name = "soap-transform"
 local cjson = require "cjson"
+local CorrelationIdHandler = {}
 
-local xml_json_transformer = require("kong.plugins.base_plugin"):extend()
-
--- constructor
-function xml_json_transformer:new()
-  xml_json_transformer.super.new(self, plugin_name)
+CorrelationIdHandler.PRIORITY = 990
+function CorrelationIdHandler:init_worker()
 end
 
-function xml_json_transformer:access(plugin_conf)
-  kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-  kong.service.request.set_header(plugin_conf.request_header, "this is on a request arulkumar")
 
+function CorrelationIdHandler:access(conf)
+  kong.service.request.set_header("Arul", "123")
 end
 
-function xml_json_transformer:header_filter(plugin_conf)
-  kong.response.set_header(plugin_conf.response_header, "this is on the response")
-end
-
-xml_json_transformer.PRIORITY = 990
-
-return xml_json_transformer
+return CorrelationIdHandler
