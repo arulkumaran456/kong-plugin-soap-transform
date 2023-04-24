cd4f81363a59845df5567b051ba729851a966267
arulkumaran456 <65466033+arulkumaran456@users.noreply.github.com>
2023-04-24 14:08:42 +0530

    Update handler.lua

--- kong/plugins/soap-transform/handler.lua
+++ kong/plugins/soap-transform/handler.lua
@@ -1,11 +1,6 @@
 local plugin_name = "soap-transform"
 local cjson = require "cjson"
 
-local plugin = {
-  PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
-  VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
-}
-
 local xml_json_transformer = require("kong.plugins.base_plugin"):extend()
 
 -- constructor
@@ -13,14 +8,16 @@ function xml_json_transformer:new()
   xml_json_transformer.super.new(self, plugin_name)
 end
 
-function plugin:access(plugin_conf)
+function xml_json_transformer:access(plugin_conf)
   kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
   kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
 
 end
 
-function plugin:header_filter(plugin_conf)
+function xml_json_transformer:header_filter(plugin_conf)
   kong.response.set_header(plugin_conf.response_header, "this is on the response")
 end
+
+xml_json_transformer.PRIORITY = 990
 
 return xml_json_transformer
