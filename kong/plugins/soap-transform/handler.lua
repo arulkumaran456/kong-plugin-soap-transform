f064cfaf0c348f9b9f327fa1fa357e730037e73a
arulkumaran456 <65466033+arulkumaran456@users.noreply.github.com>
2023-04-24 14:13:19 +0530

    Update handler.lua

--- kong/plugins/soap-transform/handler.lua
+++ kong/plugins/soap-transform/handler.lua
@@ -10,12 +10,12 @@ end
 
 function xml_json_transformer:access(plugin_conf)
   kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-  kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
+  kong.service.request.set_header(plugin_conf.request_header, "this is on a request arulkumar")
 
 end
 
 function xml_json_transformer:header_filter(plugin_conf)
-  kong.response.set_header(plugin_conf.response_header, "this is on the response")
+  kong.response.set_header(plugin_conf.response_header, "this is on the response arulkumar")
 end
 
 xml_json_transformer.PRIORITY = 990
