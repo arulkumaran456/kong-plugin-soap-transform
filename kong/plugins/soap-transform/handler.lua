48276a7441ac9d4598907b69ca777b0d9555da79
arulkumaran456 <65466033+arulkumaran456@users.noreply.github.com>
2023-04-26 08:15:28 +0530

    Update handler.lua

--- kong/plugins/soap-transform/handler.lua
+++ kong/plugins/soap-transform/handler.lua
@@ -11,15 +11,16 @@ CorrelationIdHandler.VERSION = "0.1"
 function CorrelationIdHandler:init_worker()
 end
 
+function CorrelationIdHandler:body_filter(config)
+  local body = kong.response.get_raw_body()
+  if body then
+    body = "<success>true</success>"
+    kong.response.set_raw_body(body)
+  end
+end 
+
 function CorrelationIdHandler:access(conf)
-
   kong.service.request.enable_buffering()
-end
-
-function CorrelationIdHandler:response(conf)
-  kong.service.response.get_body()
-  kong.service.response.get_raw_body()
-
 end
 
 
